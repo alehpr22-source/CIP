66fa50122818e53104883461a0a6746f42cee727
alehpr22-source <alehpr22@gmail.com>
2026-05-23 05:10:06 -0500

    cipV1

--- ginext/assets/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as phoneMockup1 } from './phone-mockup-1.png';
-export { default as phoneMockup2 } from './phone-mockup-2.png';
-export { default as phoneMockup3 } from './phone-mockup-3.png';
--- ginext/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export const Logo = () => {
-  return (
-    <Link href='/' className='text-2xl font-bold text-red-700 font-sora'>
-      Gremio de Ingenieros
-    </Link>
-  );
-};
